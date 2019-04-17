b26e3fdc3c22d9d24a472e1bb8ca30b2ef59600f
thaoho93 <none@none>
2019-04-17 20:03:10 +0700

    finish bai 5

--- src/main/java/imic/buoi10/practise10.java
+++ src/main/java/imic/buoi10/practise10.java
@@ -1,5 +1,7 @@
 package imic.buoi10;
 
+import java.util.Scanner;
+
 public class practise10 {
     public static int daySoFn(int n) {
         int Fn = 1;
@@ -56,7 +58,7 @@ public class practise10 {
     }
 
     public static void lietKeSNT(int f) {
-        System.out.println("Cac so nguyen nho hon n la ");
+        System.out.println("Cac so nguyen to nho hon n la ");
         for (int g = 1; g < f; g++) {
             int soUocf = 0;
             for (int k = 1; k <= g; k++) {
@@ -71,7 +73,88 @@ public class practise10 {
             }
         }
     }
+
+    public static void phanTichThuaSo(int h) {
+        int H = h;
+        System.out.print("Phan tich thua so cua " + h + " = ");
+        while (H > 1) {
+            for (int j = 1; j <= h; j++) {
+                if (h % j == 0) {
+                    int uoc = 0;
+                    for (int q = 1; q <= j; q++) {
+                        if (j % q == 0) {
+                            uoc++;
+                        }
+                    }
+                    if (uoc == 2) {
+                        if (H % j == 0) {
+                            H = H / j;
+                            System.out.print(j);
+                            j = 0;
+                        } else {
+                            continue;
+                        }
+                        if (H == 1) {
+                            break;
+                        } else {
+                            System.out.print(" x ");
+                        }
+                    }
+                }
+            }
+        }
+
+
+    }
+
+    public static void tongSoNguyen(int w) {
+        int sum = 0;
+        int W = w;
+        int s = 0;
+        int[] tongW = new int[w];
+        System.out.print("Tong cac chu so cua so nguyen " + w + " = ");
+        for (s = 0; W >= 1; s++) {
+            int x = W % 10;
+            W = W / 10;
+            sum += x;
+            tongW[s] = x;
+        }
+        for (int v = s - 1; v >= 0; v--) {
+            System.out.print(tongW[v]);
+            if (v == 0) {
+                break;
+            }
+            System.out.print(" + ");
+        }
+        System.out.print(" = " + sum);
+    }
+
+    public static void soThuanNghich(int y) {
+        int[] arrY = new int[y];
+        int Y = y;
+        int i = 0;
+        int j = 0;
+        for (i = 0; Y >= 1; i++) {
+            arrY[i] = Y % 10;
+            Y = Y / 10;
+        }
+        for (j = 0; j < i; j++) {
+            if (arrY[j] != arrY[i - j - 1]) {
+                System.out.println(y + " khong phai la so thuan nghich");
+                break;
+            }
+
+        }
+        if (j == i) {
+            System.out.println(y + " la so thuan nghich");
+        }
+
+    }
 }
+
+
+
+
 
 
 
