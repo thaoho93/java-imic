1b007b0f445390837e9a9178528206318df7e540
thaoho93 <none@none>
2019-05-20 20:41:53 +0700

    cuoi 20 bai b

--- src/main/java/imic/buoi20/CuaHang.java
+++ src/main/java/imic/buoi20/CuaHang.java
@@ -10,10 +10,37 @@ public class CuaHang {
         menu[1] = new TraSua();
         menu[2] = new Soda();
     }
-    public static void inMenu(){
+
+    public static void inMenu() {
         CuaHang ch = new CuaHang();
-        for (DoUong i:ch.menu){
+        for (DoUong i : ch.menu) {
             System.out.println(i.toString());
         }
+    }
+
+    public static double doanhthu() {
+        String dsBanDuoc = "CCTSCTSTTT";
+        int countC = 0;
+        int countT = 0;
+        int countS = 0;
+        for (int i = 0; i < dsBanDuoc.length(); i++) {
+            char c = dsBanDuoc.charAt(i);
+            switch (c) {
+                case 'C':
+                    countC++;
+                    break;
+                case 'T':
+                    countT++;
+                    break;
+                default:
+                    countS++;
+            }
+        }
+        System.out.println("Da ban duoc Ca phe : " + countC);
+        System.out.println("Da ban duoc Tra sua : " + countT);
+        System.out.println("Da ban duoc Soda : " + countS);
+        double doanhthu1h = countC * new CaPhe().price + countS * new Soda().price + countT * new TraSua().price;
+
+        return doanhthu1h;
     }
 }
--- src/main/java/imic/buoi20/DoUong.java
+++ src/main/java/imic/buoi20/DoUong.java
@@ -3,12 +3,12 @@ package imic.buoi20;
 public class DoUong {
     String category;
     String name;
-    double price;
+    static double price;
 
     public DoUong() {
 
     }
     public String toString() {
-        return "Category: " + category + " | Name: " + name + " | Price: " +price;
+        return "Category: " + category + " | Name: " + name + " | Price: " + price;
     }
 }
